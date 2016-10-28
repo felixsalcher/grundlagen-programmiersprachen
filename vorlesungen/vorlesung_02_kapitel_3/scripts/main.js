4564ac923da85c047d3f10eb9ef7295af6596563
Felix Salcher <felix.salcher@gmail.com>
2016-10-28 11:19:42 +0200

    vorlesung_02_start

---
+++ vorlesungen/vorlesung_02_kapitel_3/scripts/main.js
@@ -0,0 +1,14 @@
+"use strict";
+function test1() {
+    let eineZahl = 10;
+    let nochEineZahl = 20;
+
+    // Addition
+    let ergebnis = eineZahl + nochEineZahl;
+    console.log(ergebnis);
+
+    // Division
+    ergebnis = eineZahl / nochEineZahl;
+    console.log(ergebnis);
+}
+test1();
