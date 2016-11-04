8e077b134b9ea490ae6c1f2ef93b693d6d376e71
Felix Salcher <felix.salcher@gmail.com>
2016-11-04 18:19:55 +0100

    exercise for lv 3 exercise 2

--- vorlesungen/vorlesung_03_uebung/scripts/aufgabe02.js
+++ vorlesungen/vorlesung_03_uebung/scripts/aufgabe02.js
@@ -1,3 +1,50 @@
-/**
- * Created by felix on 04.11.2016.
- */
+function getImages() {
+    return [document.getElementById("image1"), document.getElementById("image2")];
+}
+
+function margin20() {
+    let images = getImages(),
+        value = "20px";
+
+    if(images[0].style.margin == value) {
+        value = "0";
+    }
+
+    images[0].style.margin = value;
+    images[1].style.margin = value;
+}
+
+function borderRed() {
+    let images = getImages(),
+        value = "none";
+
+    if(images[0].style.border == value || images[0].style.border == "") {
+        value = "5px solid #f00";
+    }
+    console.log(images[0].style.border);
+    images[0].style.border = value;
+    images[1].style.border = value;
+}
+
+function barrelRoll() {
+    let images = getImages();
+
+    images[0].className = images[1].className = "animate barrel_roll";
+
+    setTimeout(function(){
+        images[0].className = images[1].className = "";
+    }, 1000)
+}
+
+//this is the shit that happens on a boring train ride home
+function redButton() {
+    let images = getImages();
+
+    images[0].className = "stupid_zoom";
+    images[1].className = "stupid_animation";
+
+    setTimeout(function () {
+        alert("You should not have done that. What was once seen, can not be unseen!");
+        images[0].className = images[1].className = "";
+    }, 11000)
+}
