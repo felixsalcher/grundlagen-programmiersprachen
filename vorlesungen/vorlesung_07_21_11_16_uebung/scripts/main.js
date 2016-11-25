768a22fc20c2c4d5bac1f5a684fd23dd4ab6a768
Felix Salcher <felix.salcher@gmail.com>
2016-11-25 11:39:44 +0100

    updating Vorlesung_7

--- vorlesungen/vorlesung_07_21_11_16_uebung/scripts/main.js
+++ vorlesungen/vorlesung_07_21_11_16_uebung/scripts/main.js
@@ -8,6 +8,9 @@ function getElement(id) {
 
 function schleife1() {
     let ausgabe = "";
+
+    //mit break; kann die Schleife abgebrochen werden
+    //mit continue; kann die aktuelle Schleifeniteration abgebrochen und die nächste begonnen werden.
     for(let i = 1; i <= 20; i++) {
         ausgabe += i + " ";
     }
@@ -71,6 +74,14 @@ function createLoop() {
         increment = parseInt(getElement("increment").value),
         ausgabe = "";
 
+    if(increment < 0) {
+        console.log(increment);
+        increment = increment * -1;
+        console.log(increment);
+
+        document.getElementById("increment").value = increment;
+    }
+
     for(let i = startwert; i <= endwert; i += increment) {
         ausgabe += i + " ";
     }
@@ -82,8 +93,9 @@ function createTriangle() {
         ausgabe = "";
 
     for(let i = 1; i <= rows; i++) {
-
+        console.log(i);
         for(let j = 1; j <= i; j++) {
+            console.log(j);
             ausgabe += "* ";
         }
         ausgabe += "<br>";
@@ -100,7 +112,7 @@ function createChessBoard() {
 
         for(let j = 0; j < rows; j++) {
 
-            ausgabe += ((i+j) % 2 == 0) ? " * " : "&nbsp;&nbsp;&nbsp;";
+            ausgabe += ((i+j) % 2 == 0) ? "&nbsp;*&nbsp;" : "&nbsp;&nbsp;&nbsp;";
         }
         ausgabe += "<br>";
     }
@@ -115,17 +127,74 @@ function createTree() {
     for(let i = 1; i <= rows; i++) {
         //Linke Leerzeichen
         for(let j = 1; j <= rows - i; j++) {
-            ausgabe += "B";
+            ausgabe += "&nbsp;";
         }
         //Sterne
         for(let j = 1; j <= 2*i - 1; j++) {
-            ausgabe += "A";
-        }
-        //Rechte Leerzeichen
-        for(let j = 1; j <= rows - i; j++) {
-            ausgabe += "B";
+            ausgabe += "*";
         }
         ausgabe += "<br>";
     }
     document.getElementById("ausgabe3").innerHTML = ausgabe;
 }
+
+function fibonacci() {
+    let ausgabe = "",
+        vorherigeZahl = 0,
+        aktuelleZahl = 1,
+        ergebnis = 0;
+
+    for(var i = 0; i<10; i++) {
+        ergebnis = vorherigeZahl + aktuelleZahl;
+        vorherigeZahl = aktuelleZahl;
+        aktuelleZahl = ergebnis;
+        ausgabe += ergebnis + " ";
+    }
+    document.getElementById("ausgabe4").innerHTML = ausgabe;
+}
+
+// **** Menüaufgabe ****
+
+var menues = [],
+    bestellung = [];
+menues.push(["Schnitzel", 15.20, "A, B, C"]);
+menues.push(["Kaischerschmarren", 13.50, "A"]);
+menues.push(["Wurstnudeln", 9.20, "G, H"]);
+
+function info() {
+    let mNR = parseInt(getElement("menu_nr").value),
+        ausgabe = getElement("ausgabe5");
+
+    ausgabe.innerHTML = "<p>Name: " + menues[mNR - 1][0] + "</p><p>Preis: " + menues[mNR - 1][1] + "</p><p>Allergene: " + menues[mNR - 1][2] + "</p>";
+}
+
+function add() {
+    let mNR = parseInt(getElement("menu_nr").value),
+        mAnz = parseInt(getElement("menu_amount").value),
+        ausgabe = getElement("ausgabe5");
+
+    if(mNR != "" && mAnz != "") {
+        bestellung.push([mNR - 1, mAnz]);
+        ausgabe.innerHTML = "<p>Bestellung über " + mAnz + " " + menues[mNR - 1][0] + " erfolgreich hinzugefügt!";
+    }
+    console.log(bestellung);
+}
+
+function show() {
+    let ausgabe = getElement("ausgabe5"),
+        ergebnis = "",
+        product = [],
+        gesamtpreis = 0;
+
+    for(let i = 0; i < bestellung.length; i++) {
+        product = menues[bestellung[i][0]];
+        ergebnis += "<p>" + bestellung[i][1] + " " + product[0] + " á " + product[1] + "</p>";
+        gesamtpreis += product[1] * bestellung[i][1];
+        console.log(ergebnis);
+        console.log(gesamtpreis);
+    }
+    ergebnis += "<p>Gesamtpreis: " + gesamtpreis + "</p>";
+    ausgabe.innerHTML = ergebnis;
+}
+
+// **** Menüaufgabe ****
