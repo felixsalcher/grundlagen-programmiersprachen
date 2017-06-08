1dcbea3c1c550dbf9b6fc3b70b70d75eb70688b2
Felix Salcher <felix.salcher@gmail.com>
2017-06-08 19:18:48 +0200

    adding google map stuff

--- experiments/google_map/scripts/main.js
+++ experiments/google_map/scripts/main.js
@@ -1,21 +1,65 @@
 var map;
 var geocoder;
-var cities = ["Bregenz"];
-var coordinates = [];
+var db;
 
-//TODO: Create Database
-//TODO: Create Table (id, city_name, lat, lng)
-//TODO: Read from Database and create Markers accordingly
-//TODO: Add new Citys to Database and check if the entered city is already in the database
+function openDB() {
+    db = openDatabase('berghuette', '1.0', 'Database that holds all the visitors locations', 1 * 1024 * 1024);
+}
 
+function createTable(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS locations(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, city_name TEXT NOT NULL UNIQUE, lat REAL, lng REAL);', [], SQLSuccess, SQLFail);
+}
+
+function getAllEntries(tx) {
+    tx.executeSql('SELECT * FROM locations', [], addMarkers, SQLFail);
+}
+
+function addMarkers(tx, result) {
+    var marker;
+
+    //create a marker for every entry in the database
+    for(var location in result.rows) {
+        if(result.rows.hasOwnProperty(location)) {
+            console.log(result.rows[location]);
+
+            marker = new google.maps.Marker({
+                map: map,
+                position: { lat: result.rows[location].lat, lng: result.rows[location].lng }
+            });
+        }
+    }
+}
+
+function SQLSuccess() {
+    console.log("Erfolg!");
+}
+
+function SQLFail() {
+    console.log("Misserfolg!");
+}
+
+//initMap gets called by the Google Maps Callback
 function initMap() {
+
+    //create the actual map
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 0, lng: 0 },
         zoom: 1
     });
 
+    //init geocoder to transform names into coordinates
     geocoder = new google.maps.Geocoder();
 
+    //Init DB
+    openDB();
+
+    //Create Table If not exists
+    db.transaction(createTable);
+
+    //Get all locations from the database and create Map Markers
+    db.transaction(getAllEntries);
+
+    //Event Handler
     document.getElementById('get_coordinates').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
@@ -23,46 +67,30 @@ function initMap() {
 
 function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('city_name').value;
+
     geocoder.geocode(
         { 'address': address },
         function(results, status) {
             if (status === 'OK') {
-                //resultsMap.setCenter(results[0].geometry.location);
-                let result = results[0].geometry.location;
-                let lng = result.lng();
-                let lat = result.lat();
-                let coordinate = {lat, lng};
-                coordinates.push(coordinate);
 
+                let result = results[0].geometry.location;
+                let coordinate = {lat: result.lat(), lng: result.lng()};
+
+                //add marker for new location
                 var marker = new google.maps.Marker({
                     map: resultsMap,
                     position: coordinate
                 });
-            } else {
+
+                //save new location into database
+                db.transaction(function(tx) {
+                    tx.executeSql('INSERT INTO locations(city_name, lat, lng) VALUES (?, ?, ?);', [address, lat, lng], SQLSuccess, SQLFail);
+                });
+
+            }
+            else {
                 alert('Geocode was not successful for the following reason: ' + status);
             }
         }
     );
-}
-
-function getCoordinates() {
-    cities.forEach(city => {
-        geocoder.geocode({
-            address: city
-        }, function(results, status) {
-            if(status === 'OK') {
-                let result = results[0].geometry.location;
-                let lng = result.lng();
-                let lat = result.lat();
-                let coordinate = {lat, lng};
-                coordinates.push(coordinate);
-
-                return new google.maps.Marker({
-                    map: map,
-                    position: coordinate
-                });
-            }
-        });
-    });
-    console.log(coordinates);
 }
